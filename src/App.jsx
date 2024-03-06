370074570ee83e9a840f46c51867b8229035bb88
Juanan <juanantonioamillopez@gmail.com>
2024-03-06 12:27:11 +0100

    Estilos en CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,19 @@
+import profJPG from './assets/profile.jpg';
+
 function App() {
-  return <div>Hola mundo</div>;
+
+  // TUTORIAL: https://youtu.be/7iobxzd_2wY?si=WWwxyUXAPwSEBerX&t=3836
+  return (
+    <article>
+      <header>
+        <img src={profJPG} alt='imagen de perfil' srcset='' />
+        <div>
+          <strong>JuAИAИ ∀milғ</strong>
+          <span>JuANANA1000</span>
+        </div>
+      </header>
+    </article>
+  );
 }
 
 export default App;
