bef7bccc715609cbffe27fbfe892e59244b85991
Juanan <juanantonioamillopez@gmail.com>
2024-03-07 08:39:16 +0100

    Estilos en react

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import profJPG from './assets/profile.jpg';
-
+import './App.css';
 function App() {
-
   // TUTORIAL: https://youtu.be/7iobxzd_2wY?si=WWwxyUXAPwSEBerX&t=3836
   return (
-    <article>
-      <header>
-        <img src={profJPG} alt='imagen de perfil' srcset='' />
-        <div>
+    <article className='tw-followCard'>
+      <header className='tw-followCard-header'>
+        <img className='tw-followCard-avatar' src={profJPG} alt='imagen de perfil' srcset='' />
+        <div className='tw-followCard-info'>
           <strong>JuAИAИ ∀milғ</strong>
-          <span>JuANANA1000</span>
+          <span className='tw-followCard-infoUserName'>JuANANA1000</span>
         </div>
       </header>
+
+      <aside>
+        <button className='tw-followCard-button'>Seguir</button>
+      </aside>
     </article>
   );
 }
